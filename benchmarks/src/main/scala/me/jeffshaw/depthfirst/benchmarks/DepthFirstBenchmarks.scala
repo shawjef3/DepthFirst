ead33ff5d68517c51e1bac66ea7981014cdb30c9
Jeff Shaw <shawjef3@gmail.com>
2017-08-04 16:17:57 -0400

    better benchmarks

--- benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/DepthFirstBenchmarks.scala
+++ benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/DepthFirstBenchmarks.scala
@@ -7,34 +7,54 @@ import org.openjdk.jmh.annotations.{State => JmhState, _}
 @JmhState(Scope.Thread)
 class DepthFirstBenchmarks {
 
-  @Param(Array("0","786432","1572864","2359296","3145728","3932160","4718592"))
+  @Param(Array("0", "1","16","256","4096","65536","1048576","16777216"))
   var valueCount: Int = _
 
-  var values: Array[Integer] = _
+  var values: Array[Int] = _
 
-  var valuesStream: Stream[Integer] = _
+  var valuesSeq: Seq[Int] = _
 
-  @Param(Array("1", "4", "8", "12", "16"))
+  var valuesVector: Vector[Int] = _
+
+  var valuesList: List[Int] = _
+
+  var valuesStream: Stream[Int] = _
+
+  @Param(Array("1","2","4","8"))
   var iterationCount: Int = _
 
   @Setup(Level.Iteration)
   def setup(): Unit = {
     values = java.util.Arrays.copyOf(DepthFirstBenchmarks.values, valueCount)
+    valuesSeq = values.toSeq
+    valuesList = values.toList
     valuesStream = values.toStream
+    valuesVector = values.toVector
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def classic(): Unit = {
+  def classicVector(): Unit = {
     var result = values
     for (_ <- 1 until iterationCount) {
-      result = result.flatMap(x => Array(x))
+      result = result.flatMap(x => Vector(x))
     }
     for (r <- result) r
   }
 
-  @Benchmark
+//  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def classicList(): Unit = {
+    var result = values
+    for (_ <- 1 until iterationCount) {
+      result = result.flatMap(x => List(x))
+    }
+    for (r <- result) r
+  }
+
+//  @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def stream(): Unit = {
@@ -42,29 +62,38 @@ class DepthFirstBenchmarks {
     for (_ <- 1 until iterationCount) {
       result = result.flatMap(x => Stream(x))
     }
-    //force evaluation
     for (r <- result) r
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def depthFirst(): Unit = {
-    var df = DepthFirst(values)
+  def depthFirstVector(): Unit = {
+    var result = DepthFirst(valuesVector)
     for (_ <- 1 to iterationCount) {
-      df = df.flatMap(x => Array(x))
+      result = result.flatMap(x => Vector(x))
     }
-    val i = df.toIterator
-    for (_ <- i) ()
+    for (_ <- result) ()
   }
 
-  @Benchmark
+//  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def depthFirstList(): Unit = {
+    var result = DepthFirst(valuesList)
+    for (_ <- 1 to iterationCount) {
+      result = result.flatMap(x => List(x))
+    }
+    for (_ <- result) ()
+  }
+
+//  @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def javaStream(): Unit = {
     var result = DepthFirst(values)
     for (i <- 1 until iterationCount) {
-      result = result.flatMap(x => Seq(x))
+      result = result.flatMap(x => Array(x))
     }
     for (r <- result) r
   }
@@ -72,5 +101,5 @@ class DepthFirstBenchmarks {
 }
 
 object DepthFirstBenchmarks {
-  val values: Array[Integer] = Array.fill(4718592)(util.Random.nextInt())
+  val values: Array[Int] = Array.fill(16777216)(util.Random.nextInt())
 }

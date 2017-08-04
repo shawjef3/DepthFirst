7448c5ef000e800fcb959add3de9e3effe2bea06
Jeff Shaw <shawjef3@gmail.com>
2017-08-04 11:48:30 -0400

    better benchmarks

--- benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/DepthFirstBenchmarks.scala
+++ benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/DepthFirstBenchmarks.scala
@@ -1,42 +1,26 @@
 package me.jeffshaw.depthfirst.benchmarks
 
 import java.util.concurrent.TimeUnit
-import java.util.stream.IntStream
-import me.jeffshaw.depthfirst.{DepthFirst, Op}
+import me.jeffshaw.depthfirst.DepthFirst
 import org.openjdk.jmh.annotations.{State => JmhState, _}
 
 @JmhState(Scope.Thread)
 class DepthFirstBenchmarks {
 
-  @Param(Array("0", "1", "1000", "5000", "10000", "50000", "100000", "1000000", "2000000", "3000000"))
+  @Param(Array("0","786432","1572864","2359296","3145728","3932160","4718592"))
   var valueCount: Int = _
 
-  var values: Vector[Int] = _
+  var values: Array[Integer] = _
 
-  var valuesStream: Stream[Int] = _
+  var valuesStream: Stream[Integer] = _
 
-  var valuesArray: Array[Int] = _
-
-  var valuesBoxedArray: Array[Integer] = _
-
-  //Not a var, because java streams can't be reused.
-  def javaValues: java.util.stream.IntStream = IntStream.of(valuesArray: _*)
-
-  def javaBoxedValues: java.util.stream.Stream[Integer] =
-    java.util.Arrays.stream(valuesBoxedArray)
-
-  var ops: Seq[Op] = _
-
-  @Param(Array("1", "3", "5", "10", "15", "20"))
+  @Param(Array("1", "4", "8", "12", "16"))
   var iterationCount: Int = _
 
   @Setup(Level.Iteration)
   def setup(): Unit = {
-    values = DepthFirstBenchmarks.values.take(valueCount)
+    values = java.util.Arrays.copyOf(DepthFirstBenchmarks.values, valueCount)
     valuesStream = values.toStream
-    valuesArray = values.toArray
-    valuesBoxedArray = valuesArray.map(new Integer(_))
-    ops = Seq.fill(iterationCount)(Op.FlatMap(x => Vector(x)))
   }
 
   @Benchmark
@@ -44,9 +28,10 @@ class DepthFirstBenchmarks {
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def classic(): Unit = {
     var result = values
-    for (i <- 1 to iterationCount) {
-      result = result.flatMap(x => Vector(x))
+    for (_ <- 1 until iterationCount) {
+      result = result.flatMap(x => Array(x))
     }
+    for (r <- result) r
   }
 
   @Benchmark
@@ -54,51 +39,38 @@ class DepthFirstBenchmarks {
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def stream(): Unit = {
     var result = valuesStream
-    for (i <- 1 to iterationCount) {
+    for (_ <- 1 until iterationCount) {
       result = result.flatMap(x => Stream(x))
     }
     //force evaluation
-    result.lastOption
+    for (r <- result) r
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def depthFirst(): Unit = {
-    DepthFirst.iterator[Int, Int](values, ops.head, ops.tail: _*).toVector
-  }
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.AverageTime))
-  @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def depthFirstArray(): Unit = {
-    DepthFirst.iterator[Int, Int](valuesArray, ops.head, ops.tail: _*).toArray
+    var df = DepthFirst(values)
+    for (_ <- 1 to iterationCount) {
+      df = df.flatMap(x => Array(x))
+    }
+    val i = df.toIterator
+    for (_ <- i) ()
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def javaStream(): Unit = {
-    var result = javaValues
-    for (i <- 1 to iterationCount) {
-      result = result.flatMap((x: Int) => IntStream.of(x))
+    var result = DepthFirst(values)
+    for (i <- 1 until iterationCount) {
+      result = result.flatMap(x => Seq(x))
     }
-    result.toArray()
-  }
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.AverageTime))
-  @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def javaBoxedStream(): Unit = {
-    var result = javaBoxedValues
-    for (i <- 1 to iterationCount) {
-      result = result.flatMap((x: Integer) => java.util.stream.Stream.of[Integer](x))
-    }
-    result.toArray()
+    for (r <- result) r
   }
 
 }
 
 object DepthFirstBenchmarks {
-  val values: Vector[Int] = Vector.fill(16777216)(util.Random.nextInt())
+  val values: Array[Integer] = Array.fill(4718592)(util.Random.nextInt())
 }

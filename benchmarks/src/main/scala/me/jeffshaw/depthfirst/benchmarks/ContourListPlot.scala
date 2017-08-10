ed9344e42b45e764cefd90c32df89c94bbc6b505
Jeff Shaw <shawjef3@gmail.com>
2017-08-10 15:05:20 -0400

    add benchmarks names to the graph titles I removed the sql credentials. Already changed the password.

--- benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/ContourListPlot.scala
+++ benchmarks/src/main/scala/me/jeffshaw/depthfirst/benchmarks/ContourListPlot.scala
@@ -16,30 +16,31 @@ case class ContourListPlot(
     }
   }
 
-  val mathematica: String = {
+  def mathematica(base: String, comparison: String): String = {
     val values = mathematicaValues.map(_.mkString("{", ",", "}")).mkString("{", ",", "}")
     val cacheMb = cache / 1024 / 1024
-    val plotLabel = "\"" + s"cache ${cacheMb}MB, duplication factor $duplicationFactor" + "\""
+    val plotLabel = "\"" + s"$base vs $comparison, cache ${cacheMb}MB, duplication factor $duplicationFactor" + "\""
     s"""<| "values" ->  $values, "plotLabel" -> $plotLabel |>"""
   }
 }
 
 object ContourListPlot {
 
-  val list = Select[ContourListPlot](
-    """select cpu, cache, duplication_factor AS duplicationFactor, array_agg(ROW("values", iterations, improvement))
-      |from compare('DepthFirstBenchmarks.classicVector', 'DepthFirstBenchmarks.stackDepthFirstVector')
-      |group by cpu, cache, duplication_factor
-      |order by cache, duplication_factor
-      |""".stripMargin
-  )
+  def list(base: String, comparison: String): Select[ContourListPlot] =
+    Select[ContourListPlot](
+      s"""select cpu, cache, duplication_factor AS duplicationFactor, array_agg(ROW("values", iterations, improvement))
+         |from compare('DepthFirstBenchmarks.$base', 'DepthFirstBenchmarks.$comparison')
+         |group by cpu, cache, duplication_factor
+         |order by cache, duplication_factor
+         |""".stripMargin
+    )
 
   def main(args: Array[String]): Unit = {
     val h = new HikariConfig()
-    h.setJdbcUrl("jdbc:postgresql://psql.jeffshaw.me/data_local_flatmap?user=postgres&password=ofPMstLSqmsGr4r5")
+    h.setJdbcUrl(args(0))
     val pool = Pool(h)
     pool.withConnection {implicit connection =>
-      val dataSets = list.vector().map(_.mathematica).mkString("{", ",", "}")
+      val dataSets = list(args(1), args(2)).vector().map(_.mathematica(args(1), args(2))).mkString("{", ",", "}")
       println(
         s"""dataSets := $dataSets
            |
@@ -47,7 +48,7 @@ object ContourListPlot {
            | #values,
            |FrameLabel -> {Elements, FlatMaps},
            | PlotLegends -> BarLegend[Automatic, LegendLabel -> "% Improvement"],
-           | ContourLabels -> True, PlotLabel -> #plotLabel]& /@ dataSets
+           | ContourLabels -> True, PlotLabel -> #plotLabel, ImageSize -> Medium]& /@ dataSets
            |""".stripMargin
       )
     }
